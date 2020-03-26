516cf97986f70bbb82152c0d7ffb1c10ba51c698
bootstraponline <code@bootstraponline.com>
2020-03-25 17:47:23 -0700

    Refactor methods into BuildScanUtils class

--- app/enterprise.gradle.kts
+++ app/enterprise.gradle.kts
@@ -13,58 +13,56 @@ buildscript {
 
 pluginManager.apply(GradleEnterprisePlugin::class)
 
-gradleEnterprise {
-    buildScan {
-        val runningOnJenkins = "JENKINS_URL" in System.getenv()
+// https://github.com/gradle/gradle-build-scan-snippets/blob/master/guided-trials-default-custom-user-data/default-custom-user-data.gradle
+class BuildScanUtils(val buildScan: com.gradle.scan.plugin.BuildScanExtension) {
+    val runningOnJenkins = "JENKINS_URL" in System.getenv()
 
-        // -- <helpers>
+    fun isCi(): Boolean {
+        return runningOnJenkins
+    }
+
+    fun encodeURL(url: String): String {
+        return java.net.URLEncoder.encode(url, "UTF-8")
+    }
+
+    fun appendIfMissing(str: String, suffix: String): String {
+        return if (str.endsWith(suffix)) str else str + suffix
+    }
+
+    fun customValueSearchUrl(search: Map<String, String>): String {
+        val query = search.map { (name, value) ->
+            "search.names=${encodeURL(name)}&search.values=${encodeURL(value)}"
+        }.joinToString("&")
+        return "${appendIfMissing(buildScan.server, "/")}scans?$query"
+    }
+
+    fun addCustomValueSearchLink(title: String, search: Map<String, String>) {
+        if (buildScan.server.isNullOrBlank().not()) {
+            buildScan.link(title, customValueSearchUrl(search))
+        }
+    }
+
+    fun trimAtEnd(str: String): String {
+        return ("x$str").trim().substring(1)
+    }
+
+    fun execAndGetStdout(args: Array<String>): String {
+        val stdout = java.io.ByteArrayOutputStream()
+        exec {
+            commandLine(*args)
+            standardOutput = stdout
+            workingDir = rootDir
+        }
+        return trimAtEnd(stdout.toString())
+    }
+
+    fun tagOs() {
+        buildScan.tag(System.getProperty("os.name"))
+    }
+
+    fun tagIde() {
+        // TODO: broken?
         // https://github.com/gradle/gradle-build-scan-snippets/blob/master/guided-trials-default-custom-user-data/default-custom-user-data.gradle
-        fun isCi(): Boolean {
-            return runningOnJenkins
-        }
-
-        fun encodeURL(url: String): String {
-            return java.net.URLEncoder.encode(url, "UTF-8")
-        }
-
-        fun appendIfMissing(str: String, suffix: String): String {
-            return if (str.endsWith(suffix)) str else str + suffix
-        }
-
-        fun customValueSearchUrl(search: Map<String, String>): String {
-            val query = search.map { (name, value) ->
-                "search.names=${encodeURL(name)}&search.values=${encodeURL(value)}"
-            }.joinToString("&")
-            return "${appendIfMissing(buildScan.server, "/")}scans?$query"
-        }
-
-        fun addCustomValueSearchLink(title: String, search: Map<String, String>) {
-            if (buildScan.server.isNullOrBlank().not()) {
-                buildScan.link(title, customValueSearchUrl(search))
-            }
-        }
-
-        fun trimAtEnd(str: String): String {
-            return ("x$str").trim().substring(1)
-        }
-
-        fun execAndGetStdout(args: Array<String>): String {
-            val stdout = java.io.ByteArrayOutputStream()
-            exec {
-                commandLine(*args)
-                standardOutput = stdout
-                workingDir = rootDir
-            }
-            return trimAtEnd(stdout.toString())
-        }
-
-        fun tagOs() {
-            buildScan.tag(System.getProperty("os.name"))
-        }
-
-        fun tagIde() {
-            // TODO: broken?
-            // https://github.com/gradle/gradle-build-scan-snippets/blob/master/guided-trials-default-custom-user-data/default-custom-user-data.gradle
 //            if (project.hasProperty("android.injected.invoked.from.ide")) {
 //                buildScan.tag("Android Studio")
 //            } else if (System.getProperty("idea.version") != null) {
@@ -72,91 +70,101 @@ gradleEnterprise {
 //            } else if (!isCi()) {
 //                buildScan.tag("Cmd Line")
 //            }
+    }
+
+    fun tagCiOrLocal() {
+        buildScan.tag(if (isCi()) "CI" else "local")
+    }
+
+    fun addJenkinsMetadata() {
+        val buildUrl = System.getenv("BUILD_URL")
+        if (buildUrl != null) {
+            buildScan.link("Jenkins build", buildUrl)
         }
 
-        fun tagCiOrLocal() {
-            buildScan.tag(if (isCi()) "CI" else "local")
+        val buildNumber = System.getenv("BUILD_NUMBER")
+        if (buildNumber != null) {
+            buildScan.value("CI build number", buildNumber)
         }
 
-        fun addJenkinsMetadata() {
-            val buildUrl = System.getenv("BUILD_URL")
-            if (buildUrl != null) {
-                buildScan.link("Jenkins build", buildUrl)
-            }
-
-            val buildNumber = System.getenv("BUILD_NUMBER")
-            if (buildNumber != null) {
-                buildScan.value("CI build number", buildNumber)
-            }
-
-            val nodeName = System.getenv("NODE_NAME")
-            if (nodeName != null) {
-                val agentName = if (nodeName == "master") "master-node" else nodeName
-                buildScan.tag(agentName)
-                buildScan.value("CI node name", agentName)
-            }
-
-            val jobName = System.getenv("JOB_NAME")
-            if (jobName != null) {
-                val jobNameLabel = "CI job"
-                buildScan.value(jobNameLabel, jobName)
-                addCustomValueSearchLink("CI job build scans", mapOf(jobNameLabel to jobName))
-            }
-
-            val stageName = System.getenv("STAGE_NAME")
-            if (stageName != null) {
-                val stageNameLabel = "CI stage"
-                buildScan.value(stageNameLabel, stageName)
-                addCustomValueSearchLink("CI stage build scans", mapOf(stageNameLabel to stageName))
-            }
+        val nodeName = System.getenv("NODE_NAME")
+        if (nodeName != null) {
+            val agentName = if (nodeName == "master") "master-node" else nodeName
+            buildScan.tag(agentName)
+            buildScan.value("CI node name", agentName)
         }
 
-        fun addGitMetadata() {
-            buildScan.background {
-                val gitCommitId = execAndGetStdout(arrayOf("git", "rev-parse", "--short=8", "--verify", "HEAD"))
-                val gitBranchName = execAndGetStdout(arrayOf("git", "rev-parse", "--abbrev-ref", "HEAD"))
-                val gitStatus = execAndGetStdout(arrayOf("git", "status", "--porcelain"))
+        val jobName = System.getenv("JOB_NAME")
+        if (jobName != null) {
+            val jobNameLabel = "CI job"
+            buildScan.value(jobNameLabel, jobName)
+            addCustomValueSearchLink("CI job build scans", mapOf(jobNameLabel to jobName))
+        }
 
-                if (gitCommitId.isNotBlank()) {
-                    val commitIdLabel = "Git commit id"
-                    value(commitIdLabel, gitCommitId)
-                    addCustomValueSearchLink("Git commit id build scans", mapOf(commitIdLabel to gitCommitId))
-                    val originUrl = execAndGetStdout(arrayOf("git", "config", "--get", "remote.origin.url"))
-                    if (originUrl.contains("github.com")) { // only for GitHub
-                        val repoRgx = "(.*)github\\.com[\\/|:](.*)".toRegex()
-                        val repoRgxMatch = repoRgx.matchEntire(originUrl)
-                        var repoPath = repoRgxMatch?.groupValues?.get(2) ?: ""
+        val stageName = System.getenv("STAGE_NAME")
+        if (stageName != null) {
+            val stageNameLabel = "CI stage"
+            buildScan.value(stageNameLabel, stageName)
+            addCustomValueSearchLink("CI stage build scans", mapOf(stageNameLabel to stageName))
+        }
+    }
 
-                        if (repoPath.endsWith(".git")) {
-                            repoPath = repoPath.substring(0, repoPath.length - 4)
-                        }
-                        link("Github Source", "https://github.com/$repoPath/tree/$gitCommitId")
+    fun addGitMetadata() {
+        buildScan.background {
+            val gitCommitId =
+                execAndGetStdout(arrayOf("git", "rev-parse", "--short=8", "--verify", "HEAD"))
+            val gitBranchName =
+                execAndGetStdout(arrayOf("git", "rev-parse", "--abbrev-ref", "HEAD"))
+            val gitStatus = execAndGetStdout(arrayOf("git", "status", "--porcelain"))
+
+            if (gitCommitId.isNotBlank()) {
+                val commitIdLabel = "Git commit id"
+                value(commitIdLabel, gitCommitId)
+                addCustomValueSearchLink(
+                    "Git commit id build scans",
+                    mapOf(commitIdLabel to gitCommitId)
+                )
+                val originUrl =
+                    execAndGetStdout(arrayOf("git", "config", "--get", "remote.origin.url"))
+                if (originUrl.contains("github.com")) { // only for GitHub
+                    val repoRgx = "(.*)github\\.com[\\/|:](.*)".toRegex()
+                    val repoRgxMatch = repoRgx.matchEntire(originUrl)
+                    var repoPath = repoRgxMatch?.groupValues?.get(2) ?: ""
+
+                    if (repoPath.endsWith(".git")) {
+                        repoPath = repoPath.substring(0, repoPath.length - 4)
                     }
-                }
-
-                if (gitBranchName.isNotBlank()) {
-                    tag(gitBranchName)
-                    value("Git branch", gitBranchName)
-                }
-
-                if (gitStatus.isNotBlank()) {
-                    tag("Dirty")
-                    value("Git status", gitStatus)
+                    link("Github Source", "https://github.com/$repoPath/tree/$gitCommitId")
                 }
             }
-        }
 
-        // -- </helpers>
+            if (gitBranchName.isNotBlank()) {
+                tag(gitBranchName)
+                value("Git branch", gitBranchName)
+            }
+
+            if (gitStatus.isNotBlank()) {
+                tag("Dirty")
+                value("Git status", gitStatus)
+            }
+        }
+    }
+}
+
+
+gradleEnterprise {
+    buildScan {
+        val scan = BuildScanUtils(buildScan)
 
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
 
-        if (runningOnJenkins) {
-            tagOs()
-            tagIde()
-            tagCiOrLocal()
-            addJenkinsMetadata()
-            addGitMetadata()
+        if (scan.runningOnJenkins) {
+            scan.tagOs()
+            scan.tagIde()
+            scan.tagCiOrLocal()
+            scan.addJenkinsMetadata()
+            scan.addGitMetadata()
 
             publishAlways()
             isCaptureTaskInputFiles = true
@@ -172,7 +180,7 @@ gradleEnterprise {
             }
 
             // Jenkins metadata
-            addJenkinsMetadata()
+            scan.addJenkinsMetadata()
         }
 
         buildFinished {
